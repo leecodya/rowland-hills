466f398286cc887c06e899404e6b478306fd1f88
leecodya <lee.codya@gmail.com>
2020-09-13 21:44:50 -0500

    work to home banner

--- app.py
+++ app.py
@@ -2,27 +2,27 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 business_name = "Rowland Hills Weddings & Events"
-logo = "We Make Your Location Your Destination"
+moto = "We Make Your Location Your Destination"
 
 @app.route("/")
 def home():
-    return render_template("home.html", name=business_name, logo=logo)
+    return render_template("home.html", name=business_name, moto=moto)
 
 @app.route("/about")
 def about():
-    return render_template("about.html", name=business_name, logo=logo)
+    return render_template("about.html", name=business_name, moto=moto)
 
 @app.route("/gallery")
 def gallery():
-    return render_template("gallery.html", name=business_name, logo=logo)
+    return render_template("gallery.html", name=business_name, moto=moto)
 
 @app.route("/services")
 def services():
-    return render_template("services.html", name=business_name, logo=logo)
+    return render_template("services.html", name=business_name, moto=moto)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html", name=business_name, logo=logo)
+    return render_template("contact.html", name=business_name, moto=moto)
 
 if __name__ == "__main__":
     app.run(debug=True)
